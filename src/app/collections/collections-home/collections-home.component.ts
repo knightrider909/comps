463a9eadb52bd2e08d66fa7e4915ee294602a76b
Giulio <62805702+knightrider909@users.noreply.github.com>
2021-01-25 18:54:00 +0000

    collections oage changes

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'james', age: 24, job: 'marketing', employed: true },
+    { name: 'craig', age: 29, job: 'Rugby player', employed: true },
+    { name: 'aurther', age: 35, job: 'king', employed: false },
+  ];
 
-  constructor() { }
+  headers = [
+    { key: 'name', label: 'name' },
+    { key: 'age', label: 'age' },
+    { key: 'job', label: 'job' },
+    { key: 'employed', label: 'current job' },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
